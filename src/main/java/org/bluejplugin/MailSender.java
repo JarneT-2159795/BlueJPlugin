53739f230dff4b666c70f580fb91e2c7b27fa7ee
JarneT-2159795 <92175624+JarneT-2159795@users.noreply.github.com>
2023-03-30 14:16:15 +0200

    Add MailSender

---
+++ src/main/java/org/bluejplugin/MailSender.java
@@ -0,0 +1,152 @@
+package org.bluejplugin;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import org.apache.commons.codec.binary.Base64;
+
+/* Class to demonstrate the use of Gmail Create Draft with attachment API */
+public class MailSender {
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    /**
+     * Directory to store authorization tokens for this application.
+     */
+    private static final String TOKENS_DIRECTORY_PATH;
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_COMPOSE);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    private static final NetHttpTransport HTTP_TRANSPORT;
+    private static final Gmail service;
+    private static final String sender;
+    public static final String name;
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            TOKENS_DIRECTORY_PATH = BlueJManager.getInstance().getBlueJ().getUserConfigDir().getAbsolutePath();
+            service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials())
+                    .setApplicationName("BlueJPlugin")
+                    .build();
+            sender = service.users().getProfile("me").execute().getEmailAddress();
+            // parse name from email address: first.last@mail.com
+            String firstName = sender.split("@")[0].split("\\.")[0];
+            String lastName = sender.split("@")[0].split("\\.")[1];
+            name = firstName.substring(0, 1).toUpperCase() + firstName.substring(1) + " " +
+                    lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Create a draft email with attachment.
+     *
+     * @param toEmailAddress   - Email address of the recipient.
+     * @param file             - Path to the file to be attached.
+     * @throws MessagingException - if a wrongly formatted address is encountered.
+     * @throws IOException        - if service account credentials file not found.
+     */
+    public static void sendMail(String toEmailAddress,
+                                String subject,
+                                String bodyText,
+                                File file)
+            throws MessagingException, IOException {
+        // Encode as MIME message
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(sender));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(toEmailAddress));
+        email.setSubject(subject);
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(bodyText, "text/plain");
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+        mimeBodyPart = new MimeBodyPart();
+        DataSource source = new FileDataSource(file);
+        mimeBodyPart.setDataHandler(new DataHandler(source));
+        mimeBodyPart.setFileName(file.getName());
+        multipart.addBodyPart(mimeBodyPart);
+        email.setContent(multipart);
+
+        // Encode and wrap the MIME message into a gmail message
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        email.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+
+        try {
+            message = service.users().messages().send("me", message).execute();
+        } catch (GoogleJsonResponseException e) {
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 403) {
+                System.err.println("Unable to create draft: " + e.getDetails());
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials()
+            throws IOException {
+        // Load client secrets.
+        InputStream in = MailSender.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                MailSender.HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        //returns an authorized Credential object.
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+}
--- src/main/java/org/bluejplugin/QuestionWindow.java
+++ src/main/java/org/bluejplugin/QuestionWindow.java
@@ -22,9 +22,8 @@ import java.util.ArrayList;
 public class QuestionWindow
 {
     private final StackPane root;
-    private final String SERVER = "http://localhost:5000/";
-    private final char GROUPS;
-    private TextField txtName;
+    private final String SERVER = "http://127.0.0.1:8787/";
+    private final char GROUPS = 'H';
     private ComboBox<Character> cmbGroup;
     private TextArea txtQuestion;
 
@@ -32,22 +31,6 @@ public class QuestionWindow
     {
         try
         {
-            URL url = new URL(SERVER + "groups");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null)
-            {
-                response.append(inputLine);
-            }
-            in.close();
-            connection.disconnect();
-            JSONParser parser = new JSONParser();
-            JSONObject json = (JSONObject) parser.parse(response.toString());
-            GROUPS = json.get("groups").toString().charAt(0);
-
             Stage stage = new Stage();
             stage.setTitle("Vraag stellen");
             root = new StackPane();
@@ -66,9 +49,6 @@ public class QuestionWindow
         VBox vbox = new VBox();
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
-
-        txtName = new TextField();
-        vbox.getChildren().add(new HBox(new Label("Naam: "), txtName));
 
         ArrayList<Character> groups = new ArrayList<>();
         for (int i = 'A'; i <= GROUPS; i++)
@@ -91,7 +71,7 @@ public class QuestionWindow
 
     private void sendQuestion()
     {
-        if (txtName.getText().isEmpty() || cmbGroup.getValue() == null || txtQuestion.getText().isEmpty())
+        if (cmbGroup.getValue() == null || txtQuestion.getText().isEmpty())
         {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Fout");
@@ -102,58 +82,19 @@ public class QuestionWindow
         try
         {
             String pkgDir = BlueJManager.getInstance().getBlueJ().getCurrentPackage().getDir().getAbsolutePath();
-            try (FileWriter fileWriter = new FileWriter(pkgDir + "/question.txt")) {
-                fileWriter.write("Name: " + txtName.getText() + "\n" + "Group: " + cmbGroup.getValue() + "\n" + "Question: " + "\n" + txtQuestion.getText());
-            } catch (IOException e) {
-                System.err.println("Error writing to file: " + e.getMessage());
-                return;
-            }
             ZipUtil.pack(new File(pkgDir), new File(pkgDir + ".zip"));
             File zipFile = new File(pkgDir + ".zip");
-            FileInputStream fis = new FileInputStream(zipFile);
 
-            URL url = new URL(SERVER + "question");
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setDoOutput(true);
-            httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setRequestProperty("Content-Type", "application/octet-stream");
-            httpURLConnection.setRequestProperty("Content-Disposition", "attachment; filename=\"" + zipFile.getName() + "\"");
-
-            OutputStream outputStream = httpURLConnection.getOutputStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = fis.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-            outputStream.flush();
-            outputStream.close();
-            fis.close();
+            MailSender.sendMail("jarne.thys@student.uhasselt.be", "Nieuwe vraag",
+                    "Er is een nieuwe vraag gesteld door " + MailSender.name + " in groep " + cmbGroup.getValue() + ".\n\n" + txtQuestion.getText(),
+                    zipFile);
 
             zipFile.delete();
-            File questionFile = new File(pkgDir + "/question.txt");
-            questionFile.delete();
-
-            if (httpURLConnection.getResponseCode() != 200)
-            {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Fout");
-                alert.setHeaderText("Er is iets fout gegaan");
-                alert.showAndWait();
-                return;
-            }
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Succes");
-            alert.setHeaderText("Vraag is verstuurd");
-            alert.showAndWait();
 
             ((Stage) root.getScene().getWindow()).close();
         } catch (Exception e)
         {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Fout");
-            alert.setHeaderText("Er is iets fout gegaan");
-            alert.showAndWait();
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
     }
 }
